6b6ea8bf54f934dc3f1b010cf60e0d6f4f8c42e3
Kyung Hei Seo <seoky@oregonstate.edu>
2024-07-18 21:41:00 -0700

    add allowed hosts

--- backend/cookingproject/settings.py
+++ backend/cookingproject/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.190.24.46', 'localhost', '127.0.0.1']
 
 
 # Application definition
