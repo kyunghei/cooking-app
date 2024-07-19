d942f199776887072d8cb1f6c6a479cd8dead7be
Kyung Hei Seo <seoky@oregonstate.edu>
2024-07-18 22:50:38 -0700

    remove staticfiles directory to avoid confusion with static

--- backend/cookingproject/settings.py
+++ backend/cookingproject/settings.py
@@ -31,8 +31,7 @@ SECRET_KEY = (
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['18.190.24.46','localhost','127.0.0.1']
-
+ALLOWED_HOSTS = ['18.190.24.46', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -169,8 +168,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
