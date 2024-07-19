3c9bb95d312d0f2ac2628b61e0b2e111f42c6d0f
Kyung Hei Seo <seoky@oregonstate.edu>
2024-07-18 23:22:06 -0700

    configure to serve static files

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -30,3 +30,8 @@ urlpatterns = [
     path('recipes/<int:pk>/', RecipeUpdateDeleteView.as_view(), name='recipe-detail'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
