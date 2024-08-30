8bbb661be74620748b6faac267685fdeeeea471a
Kyung Hei Seo <seoky@oregonstate.edu>
2024-08-29 22:56:28 -0700

    updated the wsgi path

--- backend/cookingproject/wsgi.py
+++ backend/cookingproject/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cookingproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'backend.cookingproject.settings')
 
 application = get_wsgi_application()
