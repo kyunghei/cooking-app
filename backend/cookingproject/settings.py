66afe39ef9ca6e84517e5ef7177fae699458f9db
Kyung Hei Seo <seoky@oregonstate.edu>
2024-09-27 16:45:14 -0700

    Updated image references and other changes

--- backend/cookingproject/settings.py
+++ backend/cookingproject/settings.py
@@ -168,7 +168,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend', 'build', 'static'),
+    os.path.join(BASE_DIR, 'static')]
 
 # Enable Whitenoise to serve static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
