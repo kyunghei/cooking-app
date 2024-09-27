1af90816d10902083a97e0a1c8a8159abb84e53c
Kyung Hei Seo <seoky@oregonstate.edu>
2024-09-27 16:14:59 -0700

    Remove unused imports

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,5 @@
-import { Link, useNavigate, useLocation } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import '../App.css'
 
 
