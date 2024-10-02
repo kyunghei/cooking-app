ade4a932f285f7f01d2d7b0a8f31b4c59515bd9d
Kyung Hei Seo <seoky@oregonstate.edu>
2024-10-01 20:04:38 -0700

    Fixed error in URL path

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/';
+const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000';
 
 function Login() {
     const navigate = useNavigate();
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/';
+const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000';
 
 function Register() {
     const [formData, setFormData] = useState({
