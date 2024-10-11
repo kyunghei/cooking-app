8331560e4baf8d8da7173c6aaeb421a779d36f4e
Kyung Hei Seo <seoky@oregonstate.edu>
2024-10-11 00:13:09 -0700

    Had media URL served before the catch-all route

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -31,7 +31,9 @@ urlpatterns = [
     path('recipes/<int:pk>/update/',
          RecipeUpdateDeleteView.as_view(), name='recipe-detail'),
 
-    # Catch-all route for frontend (React)
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Catch-all route for frontend (React)
+urlpatterns += [
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'))
+]
