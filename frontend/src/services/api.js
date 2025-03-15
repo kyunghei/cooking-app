ae349294867bf0da073cf698414da2e86f7b6ad5
Kyung Hei Seo <seoky@oregonstate.edu>
2025-03-14 17:45:57 -0700

    Fix API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 import { refreshToken, getAuthHeader } from './auth';
 
 
-// const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/';
-const API_URL = 'http://127.0.0.1:8000/';
+const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/';
 
 const apiClient = axios.create({
     baseURL: API_URL
