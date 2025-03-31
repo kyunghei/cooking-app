b3be06605aa866fa6d8d842a821fcb6e64d785f0
Kyung Hei Seo <seoky@oregonstate.edu>
2025-03-31 01:34:25 -0700

    EC-29: Add catch-all route at end of URL patterns

--- backend/cookingproject/urls.py
+++ backend/cookingproject/urls.py
@@ -14,17 +14,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include  # type: ignore
+from django.urls import path, include, re_path  # type: ignore
 from rest_framework.authtoken.views import obtain_auth_token  # type: ignore
 from django.contrib import admin  # type: ignore
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("api.urls")),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Catch-all route for your React frontend:
+urlpatterns += [
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
